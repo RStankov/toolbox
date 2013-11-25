b16ce12640191520f046a6174da49e4bec067af7
Radoslav Stankov <rstankov@gmail.com>
2013-11-25 15:01:35 +0200

    Add the actuall matcher

---
+++ tools/render_json_matcher.rb
@@ -0,0 +1,15 @@
+RSpec::Matchers.define :render_json do |expected|
+  expected = expected.to_json if expected.kind_of? Hash
+
+  match do |response|
+    actual = ActiveSupport::JSON.decode(response.body).with_indifferent_access
+    match  = ActiveSupport::JSON.decode(expected).with_indifferent_access
+
+    actual.diff(match) == {}
+  end
+
+  failure_message_for_should do |response|
+    "Expected to render JSON: #{expected}\n" +
+    "      actually rendered: #{response.body}"
+  end
+end
