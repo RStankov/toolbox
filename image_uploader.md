aa7512a29140490a2f0f6445fe209cd058c3d8f2
Radoslav Stankov <rstankov@gmail.com>
2013-12-01 04:44:20 +0200

    Add ImageUploader

---
+++ image_uploader.md
@@ -0,0 +1,23 @@
+# Matcher for matching json responses
+
+## Installation
+
+* copy ```image_uploader.rb``` to ```app/models/uploaders/image_uploader.rb```
+
+## Usage
+
+```ruby
+class PhotoImageUploader < ImageUploader
+  version :main
+
+  version :thumbnail do
+    process resize_to_fill: [250, 180]
+  end
+
+end
+```
+
+## Notice
+
+```Feature.s3_uploads``` indicates if what store use are using. In general this class just easy to get start with and should be adjusts to application specific needs
+
---
+++ tools/image_uploader.rb
@@ -0,0 +1,27 @@
+class ImageUploader < CarrierWave::Uploader::Base
+  IMAGE_TYPES = %w(jpg jpeg png gif)
+
+  include CarrierWave::RMagick
+  include CarrierWave::MimeTypes
+
+  storage Features.s3_uploads? ? :fog : :file
+
+  process :set_content_type
+
+  def store_dir
+    "uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
+  end
+
+  def default_url
+    "/fallbacks/#{model.class.to_s.underscore}/#{mounted_as}/#{version_name || 'default'}.png"
+  end
+
+  def filename
+    'image.jpg' if original_filename
+  end
+
+  def extension_white_list
+    IMAGE_TYPES
+  end
+end
+
